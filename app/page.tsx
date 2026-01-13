0c4df4d322063d62c13c78243a9abf02f2e3d8a7
Alicar automotriz <yorslaura@gmail.com>
2026-01-13 12:29:54 -0500

    n

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
-"hola mundo"
+'use client'
+import { useState } from "react"
+import { text } from "stream/consumers"
 export default function Home() {
-  return(
-    <div className="p-10 font-sans">
-      <h1 className="text-2x1 font-bold
-      text-blue-600"></h1>
-      <p>Si veo esto ya no hay errores viejos</p>
+  const [ruta, setRuta] =useState('menu-principal')
+  return (
+    <div style={{ padding: '20px'}}>
+      <nav style={{ marginBottom: '20px', display: 'flex',gap: '10px'}}>
+          <button onClick={() =>
+          setRuta('menu-principal')}>Inicio</button>
+          <button onClick={() =>
+          setRuta('pagina-taller')}>Ir a taller</button>
+          <button onClick={() =>
+          setRuta('pagina-bodega')}>Ir a bodega</button>
+      </nav>
+      <hr />
+      {ruta === 'menu-principal' && (
+        <section>
+          <h1>Bienvenido al Sistema</h1>
+          <p>Elije una opcion arriba para navegar.</p>
+        </section>
+      )}
+      {ruta === 'pagina-taller' && (
+        <section>
+          <h1>Sector: Taller mecanico</h1>
+          <button onClick={() =>
+            alert('Registrando...')}>Boton de prueba en Taller</button>
+        </section>
+      )}
+      {ruta === 'pagina-bodega' && (
+        <section>
+          <h1>Bodega de repuestos</h1>
+          <input type="text" placeholder="Buscar repuesto..."/>
+        </section>
+      )}
     </div>
   )
 }
