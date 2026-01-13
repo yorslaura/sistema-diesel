4f7209af5adacf811e44a13c5bbb9f0f224564a0
Alicar automotriz <yorslaura@gmail.com>
2026-01-13 11:42:16 -0500

    n

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,10 @@
-<button className="bg-blue-500 text-white p-4 rounded-lg">
- </button>
+"hola mundo"
+export default function Home() {
+  return(
+    <div className="p-10 font-sans">
+      <h1 className="text-2x1 font-bold
+      text-blue-600"></h1>
+      <p>Si veo esto ya no hay errores viejos</p>
+    </div>
+  )
+}
